242ced2990692606b93964a723a8747ef501e4e1
Josh Goldberg <josh@fullscreenmario.com>
2018-07-22 11:51:09 -0700

    Lint fixer uppers

--- src/animator.ts
+++ src/animator.ts
@@ -1,14 +1,35 @@
 import { EmojiActor } from "./actor";
 
+/**
+ * Runs the regular gameplay loop of telling actors to animate.
+ *
+ * Each game "tick" is scheduled using `requestAnimationFrame`.
+ * During each tick, each actor is told to `act` with the time elapsed.
+ * If it indicates that it's out of bounds, it's removed from the actors array.
+ */
 export class Animator {
+    /**
+     * Actors that have been added and not yet marked themselves as out of bounds.
+     */
     private readonly actors: EmojiActor[] = [];
 
-    private previousTime = -Infinity;
+    /**
+     * Most recently time recorded by `requestAnimationFrame`.
+     */
+    private previousTime!: number;
 
+    /**
+     * Adds a new actor to act on each tick.
+     *
+     * @param actor   Newly created actor to add.
+     */
     public add(actor: EmojiActor) {
         this.actors.push(actor);
     }
 
+    /**
+     * Starts gameplay and requests the first tick.
+     */
     public start(): this {
         this.previousTime = performance.now();
 
@@ -17,6 +38,11 @@ export class Animator {
         return this;
     }
 
+    /**
+     * Runs game logic for one tick.
+     *
+     * @param currentTime   Current time, in milliseconds since page load.
+     */
     private readonly tick = (currentTime: number): void => {
         const timeElapsed = currentTime - this.previousTime;
 
--- src/utils.ts
+++ src/utils.ts
@@ -3,11 +3,27 @@ export const obtainValue = <T>(value: T | (() => T)): T =>
         ? value()
         : value;
 
-export const randomArrayMember = <T>(array: T[]): T => {
-    return array[(Math.random() * array.length) | 0];
+export const randomArrayMember = <T>(array: ReadonlyArray<T>): T => {
+    return array[Math.floor((Math.random() * array.length))];
 };
 
-export const shuffleArray = <T>(array: T[]): T[] => {
-    // Todo: actually shuffle;
-    return array;
+/**
+ * Creates a shuffled version of an array.
+ *
+ * @param array   Array to copy.
+ * @returns Shuffled version of the array.
+ */
+export const shuffleArray = <T>(array: ReadonlyArray<T>): T[] => {
+    // Copy the input array to preserve immutability elsewhere
+    const copiedArray = array.slice();
+
+    for (let i = copiedArray.length - 1; i > 0; i--) {
+        const swappingIndex = Math.floor(Math.random() * (i + 1));
+        const swapper = copiedArray[i];
+
+        copiedArray[i] = copiedArray[swappingIndex];
+        copiedArray[swappingIndex] = swapper;
+    }
+
+    return copiedArray;
 };
