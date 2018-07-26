ca13b621f503a97b34909a08b98ab0fcec209610
Josh Goldberg <josh@fullscreenmario.com>
2018-07-26 11:01:56 -0700

    Improved onclick to handle drags

--- src/mains/onclick.ts
+++ src/mains/onclick.ts
@@ -1,10 +1,25 @@
 import { emojisplosion } from "../emojisplosion";
 
-window.addEventListener("click", (event) => {
+const triggerEmojisplosion = (x: number, y: number) => {
     emojisplosion({
-        position: {
-            x: event.x,
-            y: event.y,
-        },
+        position: { x, y },
     });
+};
+
+document.addEventListener("click", (event) => {
+    triggerEmojisplosion(event.clientX, event.clientY);
+});
+
+document.addEventListener("drag", (event) => {
+    triggerEmojisplosion(event.clientX, event.clientY);
+});
+
+document.addEventListener("touchmove", (event) => {
+    for (let i = 0; i < event.touches.length; i += 1) {
+        const touch = event.touches.item(i);
+
+        if (touch !== null) {
+            triggerEmojisplosion(touch.clientX, touch.clientY);
+        }
+    }
 });
