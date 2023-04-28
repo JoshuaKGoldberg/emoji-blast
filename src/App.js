b7609d22d054a6d648159668d1bed0d48da89236
Carly Gradeff <cgradeff@gmail.com>
2023-04-28 14:14:04 -0400

    updated basic react app component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { SideBar } from './components/side-bar/side-bar';
+import { ExplosionContainer } from './components/explosion-container/explosion-container';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExplosionContainer></ExplosionContainer>
+      <SideBar></SideBar>
     </div>
   );
 }
