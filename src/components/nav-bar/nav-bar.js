29a4c5553a1dcde3df6dda9787f37922530b1e01
Carly Gradeff <cgradeff@gmail.com>
2023-04-29 11:37:18 -0400

    added icon to navbar

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+import * as styles from "./styles";
 
 export function NavBar() {
+    const ghLink = "https://github.com/JoshuaKGoldberg/emojisplosion";
+
     return (
-        <div>
+        <div style={styles.navBar}>
             <h1>emojisplosion Demo Site</h1>
+            <a href={ghLink} target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faGithub} style={styles.icon}/>
+            </a>
         </div>
     )
 }
---
+++ src/components/nav-bar/styles.js
@@ -0,0 +1,13 @@
+export const navBar = {
+    height: "4rem",
+    display: "flex",
+    justifyContent: "space-between",
+    fontFamily: "Monospace",
+    alignItems: "center",
+    padding: "0rem 2rem"
+}
+
+export const icon = {
+    color: "black",
+    fontSize: "28px"
+}
