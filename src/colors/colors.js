a57c3a2b2eb1fee2e89610b8257059f9440f9808
Carly Gradeff <cgradeff@gmail.com>
2023-04-29 21:39:32 -0400

    added custom colors

---
+++ src/colors/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+    pink: "#FA78BD",
+    orange: "#FCAD70",
+    yellow: "#F3DD7B",
+    green: "#84EB7E",
+    blue: "#73C7EB",
+    lightPurple: "#A16ED5",
+    purple: "#3A354B",
+    darkPurple: "#262139",
+    darkestPurple: "#15121E",
+}
