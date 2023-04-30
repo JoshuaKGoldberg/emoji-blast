8863d335f7cb4a11fe12fa5f5a7dd7593560ae83
Carly Gradeff <cgradeff@gmail.com>
2023-04-30 15:16:58 -0400

    added new colors

--- src/colors/colors.js
+++ src/colors/colors.js
@@ -1,6 +1,9 @@
 export const colors = {
+    white: "#ffffff",
+    gray: "#cccccc",
     pink: "#FA78BD",
     orange: "#FCAD70",
+    darkOrange: "#F89346",
     yellow: "#F3DD7B",
     green: "#84EB7E",
     blue: "#73C7EB",
