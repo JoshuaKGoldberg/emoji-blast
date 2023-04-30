3b4ce99dc074e16a743d206412efbdd4ea584378
Carly Gradeff <cgradeff@gmail.com>
2023-04-29 21:40:40 -0400

    codeblurb styling

--- src/components/code-blurb/code-blurb.js
+++ src/components/code-blurb/code-blurb.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import * as styles from "./styles";
+
 export function CodeBlurb({codeSnippet}) {
     return (
-        <>
+        <div style={styles.codeBlurb}>
             <p>{codeSnippet}</p>
-        </>
+        </div>
     )
 } 
---
+++ src/components/code-blurb/styles.js
@@ -0,0 +1,9 @@
+import { colors } from "../../colors/colors";
+
+export const codeBlurb = {
+    background: colors.darkestPurple,
+    color: "white",
+    padding: "1rem",
+    marginBottom: "2rem",
+    borderRadius: "10px"
+}
