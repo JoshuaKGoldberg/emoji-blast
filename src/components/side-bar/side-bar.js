e3df7da1463312e2518ed3e8c2553e4d37ade20c
Carly Gradeff <cgradeff@gmail.com>
2023-05-06 17:04:34 -0400

    more cleanup

--- src/components/side-bar/side-bar.js
+++ src/components/side-bar/side-bar.js
@@ -6,9 +6,10 @@ import * as emojisplosion from "../../examples/basics/emojisplosion";
 import * as numEmojis from "../../examples/basics/num-emojis";
 import * as alwaysBlue from "../../examples/basics/always-blue";
 import * as setPosition from "../../examples/basics/set-position";
+import * as titleExplosion from "../../examples/basics/title-explosion";
 import * as large from "../../examples/basics/large";
-import * as gravity from "../../examples/basics/gravity";
-import * as baseEle from "../../examples/basics/base-element";
+import * as inverseGravity from "../../examples/basics/inverse-gravity";
+import * as spinnin from "../../examples/basics/spinnin";
 
 import * as fish from "../../examples/fun-stuff/fish";
 import * as rocket from "../../examples/fun-stuff/rocket";
@@ -25,9 +26,10 @@ export function SideBar() {
             <ExampleContainer name={numEmojis.name} blurb={numEmojis.blurb} handleClick={numEmojis.handleClick} codeSnippet={numEmojis.codeSnippet} />
             <ExampleContainer name={alwaysBlue.name} blurb={alwaysBlue.blurb} handleClick={alwaysBlue.handleClick} codeSnippet={alwaysBlue.codeSnippet} />
             <ExampleContainer name={setPosition.name} blurb={setPosition.blurb} handleClick={setPosition.handleClick} codeSnippet={setPosition.codeSnippet} />
+            <ExampleContainer name={titleExplosion.name} blurb={titleExplosion.blurb} handleClick={titleExplosion.handleClick} codeSnippet={titleExplosion.codeSnippet} />
             <ExampleContainer name={large.name} blurb={large.blurb} handleClick={large.handleClick} codeSnippet={large.codeSnippet} />
-            <ExampleContainer name={gravity.name} blurb={gravity.blurb} handleClick={gravity.handleClick} codeSnippet={gravity.codeSnippet} />
-            <ExampleContainer name={baseEle.name} blurb={baseEle.blurb} handleClick={baseEle.handleClick} codeSnippet={baseEle.codeSnippet} />
+            <ExampleContainer name={inverseGravity.name} blurb={inverseGravity.blurb} handleClick={inverseGravity.handleClick} codeSnippet={inverseGravity.codeSnippet} />
+            <ExampleContainer name={spinnin.name} blurb={spinnin.blurb} handleClick={spinnin.handleClick} codeSnippet={spinnin.codeSnippet} />
             <ExampleContainer name={fish.name} blurb={fish.blurb} handleClick={fish.handleClick} codeSnippet={fish.codeSnippet} />
             <ExampleContainer name={rocket.name} blurb={rocket.blurb} handleClick={rocket.handleClick} codeSnippet={rocket.codeSnippet} />
             <ExampleContainer name={heart.name} blurb={heart.blurb} handleClick={heart.handleClick} codeSnippet={heart.codeSnippet} />
