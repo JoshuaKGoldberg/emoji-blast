fcff57154b9c4406e8cddb5d8ab83caa1e8088d9
Carly Gradeff <cgradeff@gmail.com>
2023-05-06 16:45:16 -0400

    updated singular explosion ex

--- src/examples/basics/emojisplosion.js
+++ src/examples/basics/emojisplosion.js
@@ -2,14 +2,10 @@ import { emojisplosion } from "emojisplosion";
 
 export const name = "Singular Explosion";
 
-export const blurb = "A singular explosion using the container property to act as a base for the explosions to happen within.";
+export const blurb = "A singular explosion.";
 
-export const codeSnippet = `emojisplosion({
-    container: document.getElementById("explosion-container")
-})`;
+export const codeSnippet = `emojisplosion();`;
 
 export const handleClick = () => {
-    emojisplosion({
-        container: document.getElementById("explosion-container"),
-    })
+    emojisplosion();
 }
