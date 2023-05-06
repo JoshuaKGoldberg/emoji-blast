a120d12350b1157363cbe5994085879cf2453b32
Carly Gradeff <cgradeff@gmail.com>
2023-05-06 17:06:53 -0400

    spinnin ex added

---
+++ src/examples/basics/spinnin.js
@@ -0,0 +1,31 @@
+import { emojisplosion } from "emojisplosion";
+
+export const name = "Spinnin";
+
+export const blurb = "Emojisplosion with very quickly spinning spiral emojis.";
+
+export const codeSnippet = `emojisplosion({
+    emojis: ["🌀"],
+    physics: {
+        fontSize: {min: 20, max: 38},
+        gravity: 0.1,
+        initialVelocities: {
+            rotation: {min: -14, max: -14},
+        },
+        rotationDeceleration: 1.01,
+    }
+});`;
+
+export const handleClick = () => {
+    emojisplosion({
+        emojis: ["🌀"],
+        physics: {
+            fontSize: {min: 20, max: 48},
+            gravity: 0.1,
+            initialVelocities: {
+                rotation: {min: -14, max: -14},
+            },
+            rotationDeceleration: 1.01,
+        }
+    });
+};
