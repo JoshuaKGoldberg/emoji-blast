35a87fd33280fc1dd99d70b5952472df822202a5
Carly Gradeff <cgradeff@gmail.com>
2023-05-08 14:05:19 -0400

    shooting stars ex added

--- src/components/side-bar/side-bar.js
+++ src/components/side-bar/side-bar.js
@@ -20,6 +20,7 @@ import * as rocket from "../../examples/fun-stuff/rocket";
 import * as heart from "../../examples/fun-stuff/heart";
 import * as rainstorm from "../../examples/fun-stuff/rainstorm";
 import * as rainbow from "../../examples/fun-stuff/rainbow";
+import * as shootingStars from "../../examples/fun-stuff/shooting-stars";
 
 import * as styles from "./styles";
 
@@ -43,6 +44,7 @@ export function SideBar() {
             <ExampleContainer name={heart.name} blurb={heart.blurb} explosionFunct={heart.explosionFunct} codeSnippet={heart.codeSnippet} />
             <ExampleContainer name={rainstorm.name} blurb={rainstorm.blurb} explosionFunct={rainstorm.explosionFunct} codeSnippet={rainstorm.codeSnippet} disableButtonTime={rainstorm.disableButtonTime}/>
             <ExampleContainer name={rainbow.name} blurb={rainbow.blurb} explosionFunct={rainbow.explosionFunct} codeSnippet={rainbow.codeSnippet} disableButtonTime={rainbow.disableButtonTime}/>
+            <ExampleContainer name={shootingStars.name} blurb={shootingStars.blurb} explosionFunct={shootingStars.explosionFunct} codeSnippet={shootingStars.codeSnippet} disableButtonTime={shootingStars.disableButtonTime} />
         </div>
     )
 }
