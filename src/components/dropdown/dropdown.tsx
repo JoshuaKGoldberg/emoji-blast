987545bd9982411b8897e1941ea4bddd38b0ed83
Carly Gradeff <cgradeff@gmail.com>
2023-05-13 19:44:54 -0400

    created dropdown component

---
+++ src/components/dropdown/dropdown.tsx
@@ -0,0 +1,11 @@
+/** @jsxImportSource @emotion/react */
+
+import * as styles from "./styles";
+
+export function Dropdown() {
+    return (
+        <div css={styles.dropdownBox}>
+            <p>made with 💖 & React by Carly Gradeff</p>
+        </div>
+    );
+};
---
+++ src/components/dropdown/styles.tsx
@@ -0,0 +1,5 @@
+export const dropdownBox = {
+    display: "flex",
+    alignItems: "center",
+    margin: "0rem 1.5rem 0rem 0rem",
+};
