d61d298d5e3d72ef0de37c4feec3760e1962c911
Carly Gradeff <cgradeff@gmail.com>
2023-05-13 19:52:49 -0400

    added media styles for dropdown

--- src/components/dropdown/styles.tsx
+++ src/components/dropdown/styles.tsx
@@ -2,4 +2,8 @@ export const dropdownBox = {
     display: "flex",
     alignItems: "center",
     margin: "0rem 1.5rem 0rem 0rem",
+
+    "@media (max-width: 414px)": {
+        margin: "0rem 0rem 0rem 1.5rem",
+    }
 };
