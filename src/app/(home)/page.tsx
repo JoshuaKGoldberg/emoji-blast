b22855997651ca515f4ecf2624fd672e9259750c
Carly Gradeff <cgradeff@gmail.com>
2023-05-16 14:26:41 -0400

    created home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+/** @jsxImportSource @emotion/react */
+'use client'
+
+import { Headline } from "@/components/headline/headline";
+
+export default function HomePage() {
+    return (
+        <div>
+            <Headline />
+        </div>
+    );
+};
