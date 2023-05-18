90fb62c442a89e0281b94e9d711cfc80e6afb4cc
Carly Gradeff <cgradeff@gmail.com>
2023-05-18 10:57:02 -0400

    started creating usage container

---
+++ src/components/usage/styles.tsx
@@ -0,0 +1,6 @@
+import { colors } from "../../colors/colors";
+
+export const usageContainer = {
+    backgroundColor: colors.violet300,
+    height: "55vh"
+};
---
+++ src/components/usage/usage.tsx
@@ -0,0 +1,9 @@
+/** @jsxImportSource @emotion/react */
+
+import * as styles from "./styles";
+
+export function Usage() {
+    return (
+        <div css={styles.usageContainer}></div>
+    )
+};
