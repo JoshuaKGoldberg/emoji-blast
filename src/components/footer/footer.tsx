f1a90eeff82b37a978316eb6f21e8e2b6fa27393
Carly Gradeff <cgradeff@gmail.com>
2023-05-29 14:05:48 -0400

    created footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,21 @@
+/** @jsxImportSource @emotion/react */
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+import * as styles from "./styles";
+
+export function Footer() {
+    const ghLink = "https://github.com/cgradeff/emojisplosion-site";
+
+    return (
+        <div css={styles.footer}>
+            <div css={styles.contentContainer}>
+                <p>made with 💖 & React by Carly Gradeff</p>
+                <a href={ghLink} target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faGithub} css={styles.icon}/>
+                </a>
+            </div>
+        </div>
+    );
+};
