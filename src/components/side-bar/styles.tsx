1d031247b2415e233043385105792c814e4ea63d
Carly Gradeff <cgradeff@gmail.com>
2023-06-01 14:11:15 -0400

    fix for issue #3

--- src/components/side-bar/styles.tsx
+++ src/components/side-bar/styles.tsx
@@ -3,7 +3,7 @@ import { colors } from "../../colors/colors";
 export const sideBarContainer = {
     width: "40vw",
     background: colors.violet200,
-    overflow: "scroll",
+    overflow: "auto",
     height: "calc(100vh - 5rem)",
     
     "@media (max-width: 700px)": {
