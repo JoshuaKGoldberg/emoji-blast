d0762cb174cf9ec73a9c517511af25095c629aa3
Carly Gradeff <cgradeff@gmail.com>
2023-06-01 12:36:36 -0400

    updated nav bar to have page navigation

--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -1,16 +1,22 @@
 /** @jsxImportSource @emotion/react */
 
-import { NavBarIcons } from "../nav-bar-icons/nav-bar-icons";
+import Link from "next/link";
 import { Title } from "../title/title";
+import { Dropdown } from "../dropdown/dropdown";
 
 import * as styles from "./styles";
-
 
 export function NavBar() {
     return (
         <div css={styles.navBar}>
-            <Title addStyles={styles.title} />
-            <NavBarIcons />
+            <Link css={styles.titleLink} href="/"><Title addStyles={styles.title} /></Link>
+            <div css={styles.navContainer}>
+                <div css={styles.navLinkContainer}>
+                    <Link css={styles.navLink} href="/">Home</Link>
+                    <Link css={styles.navLink} href="/demo">Demo</Link>
+                </div>
+                <Dropdown addStyles={styles.dropdown} />
+            </div>
         </div>
     );
 };
--- src/components/nav-bar/styles.tsx
+++ src/components/nav-bar/styles.tsx
@@ -1,6 +1,9 @@
 import { colors } from "../../colors/colors";
 
 export const navBar = {
+    position: "fixed" as "fixed",
+    top: "0",
+    width: "100vw",
     height: "5rem",
     display: "flex",
     justifyContent: "space-between",
@@ -12,10 +15,44 @@ export const navBar = {
     "@media (max-width: 700px)": {
         background: colors.violet100,
         fontSize: "10px",
-        padding: "0rem 1rem",
-    }
+        position: "relative" as "relative"
+    },
+};
+
+export const titleLink = {
+    textDecoration: "none",
 };
 
 export const title = {
     fontWeight: "normal",
+};
+
+export const navContainer = {
+    marginRight: "5rem",
+};
+
+export const navLink = {
+    color: colors.white,
+    fontSize: "18px",
+    textDecoration: "none",
+    margin: "0rem 2rem",
+
+    "&:hover": {
+        color: colors.gray
+    },
+};
+
+export const navLinkContainer = {
+    "@media (max-width: 700px)": {
+        display: "none"
+    },
+};
+
+export const dropdown = {
+    // TODO: fix dropdown so that the body ele is responsive when the contents open
+    display: "none",
+
+    "@media (max-width: 700px)": {
+        display: "block"
+    },
 };
