be6ec16e7ccc641f1748cd091196ecbef78ec3cc
Carly Gradeff <cgradeff@gmail.com>
2023-06-08 13:25:59 -0400

    added footer and styles to docs page

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -2,11 +2,15 @@
 'use client'
 
 import { DocContent } from "@/components/doc-content/doc-content";
+import { Footer } from "@/components/footer/footer";
+
+import * as styles from "./styles";
 
 export default function DocsPage() {
     return (
-        <div>
+        <div css={styles.docsContainer}>
             <DocContent />
+            <Footer />
         </div>
     );
 };
---
+++ src/app/docs/styles.tsx
@@ -0,0 +1,7 @@
+export const docsContainer = {
+    marginTop: "5rem",
+
+    "@media (max-width: 700px)": {
+        margin: "0",
+    },
+};
