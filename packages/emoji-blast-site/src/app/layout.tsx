bb1f5d26953e54be956fdb638b412fb0e8e53015
Josh Goldberg <git@joshuakgoldberg.com>
2024-04-27 09:35:52 -0400

    feat(emoji-blast-site): add SEO metadata

--- packages/emoji-blast-site/src/app/layout.tsx
+++ packages/emoji-blast-site/src/app/layout.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import sitePic from "../../images/site-pic.png";
 import { NavBar } from "../components/nav-bar/nav-bar";
 import "./globals.css";
+
+const description =
+	"🎆 Blasts emoji like fireworks all up in your HTML page. 🎇";
 
 export default function RootLayout({
 	children,
@@ -16,12 +20,22 @@ export default function RootLayout({
 					rel="icon"
 				/>
 				<meta content="width=device-width, initial-scale=1" name="viewport" />
-				<meta
-					content="🎆 Blasts emoji like fireworks all up in your HTML page. 🎇"
-					name="description"
-				/>
-				<meta content="../../images/site-pic.png" name="image" />
+				<meta content={description} name="description" />
+				<meta content={sitePic.src} name="image" />
 				<title>emoji-blast</title>
+
+				<meta content={description} property="og:description" />
+				<meta content={sitePic.src} property="og:image" />
+				<meta content="emoji-blast" property="og:title" />
+				<meta content="website" property="og:type" />
+				<meta content="https://emojiblast.dev" property="og:url" />
+
+				<meta content="summary_large_image" name="twitter:card" />
+				<meta content={description} name="twitter:description" />
+				<meta content="emojiblast.dev" property="twitter:domain" />
+				<meta content={sitePic.src} name="twitter:image" />
+				<meta content="emoji-blast" name="twitter:title" />
+				<meta content="https://emojiblast.dev" property="twitter:url" />
 			</head>
 			<body suppressHydrationWarning={true}>
 				<NavBar />
