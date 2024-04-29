dc209ab86b6f093c894fb8d9b86275e77a33f7c5
Daniel Roe <daniel@roe.dev>
2024-04-29 16:31:53 +0100

    fix: unsubscribe from konami when scope disposed

--- src/runtime/konami-code.client.ts
+++ src/runtime/konami-code.client.ts
@@ -1,6 +1,13 @@
 import { initializeKonamiEmojiBlast } from 'konami-emoji-blast'
+import { onScopeDispose } from 'vue'
 import { defineNuxtPlugin, onNuxtReady } from '#app'
 
 export default defineNuxtPlugin((_nuxtApp) => {
-  onNuxtReady(initializeKonamiEmojiBlast)
+  let unsubscribe: () => void
+  onNuxtReady(() => {
+    unsubscribe = initializeKonamiEmojiBlast()
+  })
+  onScopeDispose(() => {
+    unsubscribe()
+  })
 })
