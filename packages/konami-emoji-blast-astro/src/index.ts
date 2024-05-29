edf397bc1bf2e627409a1974166ef8c0dd3faf45
Josh Goldberg <git@joshuakgoldberg.com>
2024-05-29 17:35:28 -0400

    fix: correct peerDependency behavior and global/now script for astro

--- packages/konami-emoji-blast-astro/src/index.ts
+++ packages/konami-emoji-blast-astro/src/index.ts
@@ -4,7 +4,7 @@ export function konamiEmojiBlast(): AstroIntegration {
 	return {
 		hooks: {
 			"astro:config:setup"({ injectScript }) {
-				injectScript("page", `import "konami-emoji-blast/dist/global.js";`);
+				injectScript("page", `import "konami-emoji-blast/dist/now.js";`);
 			},
 		},
 		name: "@konami-emoji-blast/astro",
