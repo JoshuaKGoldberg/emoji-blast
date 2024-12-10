fb7d18cc8c0ec8a8cbee3dc28ab62b1b17310ec7
Daniel Roe <daniel@roe.dev>
2024-12-10 22:26:14 +0000

    fix(nuxt): set vite optimizeDeps config

--- packages/konami-emoji-blast-nuxt/src/module.ts
+++ packages/konami-emoji-blast-nuxt/src/module.ts
@@ -1,12 +1,24 @@
-import { addPlugin, createResolver, defineNuxtModule } from "@nuxt/kit";
+import {
+	addPlugin,
+	createResolver,
+	defineNuxtModule,
+	extendViteConfig,
+} from "@nuxt/kit";
 
 export default defineNuxtModule({
 	meta: {
 		configKey: "konamiEmojiBlast",
 		name: "@konami-emoji-blast/nuxt",
 	},
-	setup() {
+	setup(_options, nuxt) {
 		const resolver = createResolver(import.meta.url);
+
+		// TODO: remove when fixed in `konami-code-js`: https://github.com/MachinisteWeb/konami-code-js/issues/3
+		extendViteConfig((config) => {
+			config.optimizeDeps ||= {};
+			config.optimizeDeps.include = config.optimizeDeps.include || [];
+			config.optimizeDeps.include.push("konami-emoji-blast > konami-code-js");
+		});
 
 		addPlugin(resolver.resolve("./runtime/plugins/konami-code.client"));
 	},
